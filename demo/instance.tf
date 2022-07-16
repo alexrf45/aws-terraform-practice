2350a57dc1739daa6135275ab08b240a7c7146ef
alexrf45 <fonalex45@gmail.com>
2022-07-16 16:38:43 -0400

    refactor sec group to ec2 instance

--- demo/instance.tf
+++ demo/instance.tf
@@ -1,29 +1,3 @@
-
-
-resource "aws_security_group" "terraform-ssh" {
-  name        = "terraform-ssh-sg"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.dev.id
-
-  ingress {
-    description = var.description
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = var.resource_tags
-}
-
 resource "aws_key_pair" "demo_ssh_key" {
   key_name   = var.key_name
   public_key = file("~/.ssh/demo-key.pub")
